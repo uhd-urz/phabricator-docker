b93a393f976e990d8def7c2f82d79f05a12ad66b
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-04-08 22:12:27 +0200

    Add Phabricator base image which contains PHP, plus all necessary sources, plus nullmailer

---
+++ docker/phabricator-base/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+for var in MYSQL_{USER,PASSWORD} ; do
+	if [ -z "${!var+x}" ] ; then
+		echo "${var} is not set!" >& 2
+		exit 1
+	fi
+done
+
+scriptdir="$(dirname $(realpath "$0"))"
+
+cd "${scriptdir}/phabricator"
+
+echo ">> Seed local configuration ..."
+cp /srv/phabricator/config/* conf/local/
+
+echo ">> Bootstrapping local configuration ..."
+./bin/config set mysql.host "${MYSQL_HOST:-localhost}"
+./bin/config set mysql.port "${MYSQL_PORT:-3306}"
+./bin/config set mysql.pass "${MYSQL_PASSWORD}"
+./bin/config set mysql.user "${MYSQL_USER}"
+./bin/config set storage.default-namespace "${MYSQL_NAMESPACE:-phabricator}"
+
+echo ">> Upgrading database ..."
+./bin/storage upgrade --force
