862905b4351ebf2b211109cc68f8cf388f78e7ac
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-04-11 18:09:37 +0200

    Package PHD

---
+++ docker/phabricator-phd/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+onexit() {
+	echo ">> Stopping daemons ..."
+	./bin/phd stop
+}
+
+# if command starts with an option, prepend mysqld
+if [ "$1" = start ]; then
+	set -- ./bin/phd "$@"
+fi
+
+if [ "$1" = ./bin/phd ] ; then
+	../bootstrap.sh
+
+	trap onexit EXIT
+
+	echo ">> Starting daemons: $@"
+	"$@"
+
+	echo ">> Waiting for daemons to come up ..."
+	while ! test -f /var/tmp/phd/pid/daemon.* ; do
+		sleep 1
+	done
+
+	pid="$(echo /var/tmp/phd/pid/daemon.* | sed 's/^.*daemon\.//')"
+
+	echo ">> Running watchdog ..."
+	while true ; do
+		kill -0 "${pid}" || break
+		sleep 10
+	done
+else
+	exec "$@"
+fi
